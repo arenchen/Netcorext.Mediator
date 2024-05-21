8a976e28d0a6188a38b868b8e0e7002882b3b950
Aren Chen <gucci313031@gmail.com>
2024-05-21 09:22:37 +0800

    StreamMaxSize default value

--- src/Netcorext.Mediator.Queuing.Redis/Options/RedisOptions.cs
+++ src/Netcorext.Mediator.Queuing.Redis/Options/RedisOptions.cs
@@ -4,13 +4,14 @@ namespace Netcorext.Mediator.Queuing.Redis;
 
 public class RedisOptions
 {
-    public const string DEFAULT_COMMUNICATION_CHANNEL = "notification";
-    public const int DEFAULT_SLOW_COMMAND_TIMES = 2 * 1000;
-    public const int DEFAULT_STREAM_IDLE_TIME = 5 * 1000;
-    public const int DEFAULT_STREAM_BATCH_SIZE = 50;
-    public const int DEFAULT_WORKER_TASK_LIMIT = 5;
     public const int DEFAULT_RETRY_LIMIT = 3;
-    
+    public const int DEFAULT_SLOW_COMMAND_TIMES = 150;
+    public const int DEFAULT_STREAM_BATCH_SIZE = 50;
+    public const int DEFAULT_STREAM_IDLE_TIME = 5 * 1000;
+    public const int DEFAULT_STREAM_MAX_SIZE = 65535;
+    public const int DEFAULT_WORKER_TASK_LIMIT = 5;
+    public const string DEFAULT_COMMUNICATION_CHANNEL = "notification";
+
     public string GroupName { get; } = Assembly.GetEntryAssembly()?.GetName().Name!;
     public bool GroupNewestId { get; set; }
     public string MachineName { get; set; } = Environment.GetEnvironmentVariable("HOSTNAME") ?? Environment.MachineName;
@@ -20,7 +21,7 @@ public class RedisOptions
     public int SlowCommandTimes { get; set; } = DEFAULT_SLOW_COMMAND_TIMES;
     public int? StreamIdleTime { get; set; } = DEFAULT_STREAM_IDLE_TIME;
     public int? StreamBatchSize { get; set; } = DEFAULT_STREAM_BATCH_SIZE;
-    public int? StreamMaxSize { get; set; }
+    public int? StreamMaxSize { get; set; } = DEFAULT_STREAM_MAX_SIZE;
     public int? WorkerTaskLimit { get; set; } = DEFAULT_WORKER_TASK_LIMIT;
     public int? RetryLimit { get; set; } = DEFAULT_RETRY_LIMIT;
 }
